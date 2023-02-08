3180512515904e1349d3415855198e5788198ba0
pushpa-saroj <109844041+pushpa-saroj@users.noreply.github.com>
2023-02-08 11:11:33 +0530

    Initial commit

---
+++ src/Calculator/mainFolder.js
@@ -0,0 +1,57 @@
+// import React from "react";
+// import Calculator from "./Calculator";
+// import "./Calculator.css";
+// import App from './Calculator/'
+// function App() {
+//   //   const [string, setString] = useState("");
+//   // const[result,setResult]=useState("")
+//   // const[current,setCurrent]=useState("")
+//   // const[previous,setPrevious]=useState("")
+//   // const[operand,setOperand]=useState("")
+
+//   //   const buttonArray = [];
+//   //   const operations = ["+", "-", "*", "/", "%"];
+//   //   for (let i = 0; i < 10; i++) {
+//   //     buttonArray.push(i);
+//   //   }
+
+//   //   buttonArray.push(".");
+
+//   //   const handleClick = (e) => {
+//   //     setString((prev) => prev + e.target.value);
+//   //   };
+
+//   //   const calculate=()=>{
+
+//   //   }
+//   //   return (
+//   //     <div>
+//   //
+//   //       <div>
+//   //         {buttonArray.map((el, idx) => (
+//   //           <button className="button" key={idx} value={el} onClick={handleClick}>
+//   //             {el}
+//   //           </button>
+//   //         ))}
+//   //         <button onClick={calculate} value="=">=</button>
+//   //       </div>
+//   //       <input type="text" placeholder="0" id="answer" value={string} />
+//   //     </div>
+//   //   );
+//   // }
+//   // export default App;
+
+//   // // Map can be used when you want to perform an action on each element in a
+//   // // collection, and gather the results into a new array. In JavaScript,
+//   // // the "for loop" or "nesting" can complete the same task,
+//   // // but we can use map() to create more readable functions.
+//   // // Map can be created and initialized with the new Map() syntax.
+//   return (
+//     <div>
+//       <Calculator />
+//       <h3>hello</h3>
+//     </div>
+//   );
+// }
+
+// export default App;
